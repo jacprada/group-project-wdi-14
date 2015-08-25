48274bc0d00ee87945931d04998af2beefe9067f
jacprada <jacopo.prada@gmail.com>
2015-08-25 11:15:14 +0100

    added center map function when click on marker

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -3,8 +3,10 @@ $(document).ready(function(){
 });
 
 function initialize() {
+  // Getting the map div in the html file
   var mapCanvas = document.getElementById('map');
 
+  // Setting up map options to render map of London
   var mapOptions = {
     center: new google.maps.LatLng(51.517557, -0.095624),
     zoom: 13,
@@ -12,19 +14,26 @@ function initialize() {
     mapMaker: true
   }
 
+  // Rendering desired map in selected div
   var map = new google.maps.Map(mapCanvas, mapOptions);
 
+  // Making ajax call to back-end in order to retrieve json bar data
   var ajax = $.get('http://localhost:9000/')
   .done(function(data){
     $.each(data, function(index, bar){
-      console.log(bar.name);
-      console.log(bar.lat);
-      console.log(bar.lng);
+      // console.log(bar.name);
+      // console.log(bar.lat);
+      // console.log(bar.lng);
+
+      // Setting up marker based on json bar (name, lat, lng) data
       var marker = new google.maps.Marker({
         position: {lat: bar.lat, lng: bar.lng},
         map: map,
         title: bar.name
       });
+
+      // Setting up info window based on json bar (name, image, description, facebook) data
+      // Adding Citymapper link with pre-saved adddress
       var infowindow = new google.maps.InfoWindow({
         content: '<div id="content">'+
         '<div id="siteNotice">'+
@@ -34,11 +43,15 @@ function initialize() {
         '<img src="' + bar.image + '" height="200px">' +
         '<p>' + bar.description + '</p>' +
         '<a href="' + bar.facebook_url + '" target="_blank">Facebook</a><br>' +
-        '<a href="https://citymapper.com/directions?endcoord=' + bar.lat + ',' + bar.lng + '&endname=' + bar.name +'" target="_blank">Get There</a>' +
+        '<a href="https://citymapper.com/directions?endcoord='
+        + bar.lat + ',' + bar.lng + '&endname=' + bar.name +'" target="_blank">Get There</a>' +
         '</div>'+
         '</div>'
       });
+
+      // Adding click listener to open info window when marker is clicked
       marker.addListener('click', function() {
+        map.setCenter(marker.getPosition());
         infowindow.open(map, marker);
       });
     });
