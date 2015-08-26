fe83a9c3f774064dc9c45a14b90491fa0d291a40
jacprada <jacopo.prada@gmail.com>
2015-08-26 14:11:36 +0100

    trying to render login form

--- front-end/js/login.js
+++ front-end/js/login.js
@@ -33,8 +33,47 @@ $(function(){
 
   $("#about_button").on("click", function(){
     event.preventDefault();
-    $("body").prepend("<h1>Hello Mama</h1>");
+    $.ajax({
+      type: "get",
+      url: "http://localhost:3000/loginformz",
+      dataType: "json",
+      beforeSend: function(request){
+        checkAccess(request)
+      },
+    }).done(function(data, response){
+      console.log(data);
+    });
   });
+
+
+  // $("#about_button").on("click", function(){
+  //   event.preventDefault();
+  //   $("#login_form").load("login_form.html");
+  // });
+
+
+
+
+  // $("#about_button").on("click", function(){
+  //   event.preventDefault();
+  //   $("#login_form").toogle();
+  // }
+
+  // $("#about_button").on("click", function(request){
+  //   event.preventDefault();
+  //   if checkAccess(request) {
+  //     $("#login_form").toggle();
+  //   }
+  // });
+
+  // $( document ).click(function() {
+  //   $( "div" ).effect( "bounce", "slow" );
+  // });
+
+  // $("#about_button").on("click", function(){
+  //   event.preventDefault();
+  //   $("body").prepend("<h1>Hello Mama</h1>");
+  // });
 
   $(".logout").on("click", function(){
     event.preventDefault();
